9bc775a40b416bd1ac4bc9a5b9ae8dae54ef31d1
kitsuroi <154440204+kitsuroi@users.noreply.github.com>
2024-05-21 19:30:02 +0200

    feat(script): add getRandomInt.
    create a random integer from 0 to max.

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-console.log('Hello Worlds');
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max)
+}
 
 function getComputerChoice() {
 }
+
+console.log(getRandomInt(3));
+console.log(getRandomInt(3));
+console.log(getRandomInt(3));
+console.log(getRandomInt(3));
