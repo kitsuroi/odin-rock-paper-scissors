4eda56ece0fd67dcd880c6f023ec71485dcf0dce
kitsuroi <154440204+kitsuroi@users.noreply.github.com>
2024-05-21 19:42:42 +0200

    feat(getComputerChoice): add randomInt.
    creates a random int from 0 to 3 using getRandomInt functions.

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-//getRandomInt from 0 to max
+//getRandomInt from 0 to max.
 function getRandomInt(max) {
   return Math.floor(Math.random() * max)
 }
-
+// getComputerChoice of rock, paper or scissors.
 function getComputerChoice() {
+  //generate a random integer from 0 to 3
+  const randomInt = getRandomInt(3);
+
 }
