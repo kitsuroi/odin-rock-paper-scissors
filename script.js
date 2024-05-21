8401752bf9c25780da1d00d1a52d7cc67563f444
kitsuroi <154440204+kitsuroi@users.noreply.github.com>
2024-05-21 19:55:58 +0200

    feat(script.getComputerChoice); add logic and return. choice between rock, paper, scissors from randomInt and returns choice.

--- script.js
+++ script.js
@@ -7,8 +7,19 @@ function getComputerChoice() {
   //generate a random integer from 0 to 3
   const randomInt = getRandomInt(3);
   //initialize string variable computerChoice.
+  let computerChoice = '';
   //if randomInt is 0 set computerChoice to 'rock'.
+  if (randomInt === 0) {
+    computerChoice = 'rock';
+  }
   //if randomInt is 0 set computerChoice to 'paper'.
+  else if (randomInt === 1) {
+    computerChoice = 'paper';
+  }
   //if randomInt is 0 set computerChoice to 'scissors'.
+  else {
+    computerChoice = 'scissors';
+  }
   //return value of computerChoice
+  return computerChoice;
 }
