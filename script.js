52c64c9fb0cccfb303b584b2cbad41af47acbaf4
kitsuroi <154440204+kitsuroi@users.noreply.github.com>
2024-05-22 22:26:21 +0200

    fix(script:getHumanChoice): spacing

--- script.js
+++ script.js
@@ -26,21 +26,21 @@ function getComputerChoice() {
 
 //getHumanChoice of rock, paper or scissors.
 function getHumanChoice() {
-//initialize string variable userChoice.
-let userChoice = '';
-//prompt user for choice.
-userChoice = prompt('Make your choice (rock, paper or scissors)?');
-//set userChoice to lowercase.
-userChoice = userChoice.toLowerCase()
-//check for valid choice(rock, paper or scissors).
-if (userChoice === 'rock' 
-|| userChoice === 'paper' 
-|| userChoice === 'scissors') {
-  //return userChoice if valid
-  return userChoice
-} else {
-  //retry until userChoice is valid
-  alert('Input is wrong please try again. Valid inputs are rock, paper or scissors.')
-  return getHumanChoice();
-}
+  //initialize string variable userChoice.
+  let userChoice = '';
+  //prompt user for choice.
+  userChoice = prompt('Make your choice (rock, paper or scissors)?');
+  //set userChoice to lowercase.
+  userChoice = userChoice.toLowerCase()
+  //check for valid choice(rock, paper or scissors).
+  if (userChoice === 'rock' 
+  || userChoice === 'paper' 
+  || userChoice === 'scissors') {
+    //return userChoice if valid
+    return userChoice
+  } else {
+    //retry until userChoice is valid
+    alert('Input is wrong please try again. Valid inputs are rock, paper or scissors.')
+    return getHumanChoice();
+  }
 }
